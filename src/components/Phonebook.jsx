50547c2c9217cf8db1c547dc973d67f11c549255
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-08-31 17:52:06 +0300

    add css

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,28 +1,47 @@
 import PropTypes from "prop-types";
-import css from './Phonebook.module.css'
-import {Component} from 'react'
+import css from "./Phonebook.module.css";
+import { Component } from "react";
 
 class Phonebook extends Component {
-    render() {
-        const { contacts, name } = this.props;
-        return (
-            <header>
-                <h2>Phonebook</h2>
-                <form action="#">
-                    <label htmlFor="inputForm">Name</label>
-                    <input id="inputForm" type="text" />
-                    <button type="submit">Add contact</button>
-                </form>
-            </header>
-        )
-    }
+  render() {
+    const { contacts, name } = this.props;
+    return (
+      <header>
+        <h1 className={css.titlePhonebook}>Phonebook</h1>
+        <form action="#" className={css.form}>
+          <label htmlFor="sendName" className={css.name}>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            className={css.inputName}
+            id="sendName"
+          />
+          <button type="submit" className={css.submitName}>
+            Add contact
+          </button>
+        </form>
+
+        <h1 className={css.titleContacts}>Contacts</h1>
+        <ul className={css.listContacts}>
+          <li></li>
+        </ul>
+      </header>
+    );
+  }
 }
 
 Phonebook.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-  })),
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    })
+  ),
 };
 
 export default Phonebook;
