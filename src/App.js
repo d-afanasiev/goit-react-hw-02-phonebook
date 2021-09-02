2dd0df3c1a428a7961fa3b20acc956e64c30419c
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-02 15:04:06 +0300

    add Filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import PropTypes from "prop-types";
 import { Component } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
+import Filter from "./components/Filter/Filter";
 
 class App extends Component {
   state = {
@@ -15,54 +16,42 @@ class App extends Component {
   };
 
   filterList = (e) => {
-    console.log(e.target.value);
-    if (e.target.value !== "") {
-      this.setState((prevState) => ({
-        filter: e.target.value,
-        contacts: prevState.contacts.filter((item) =>
-          item.name.toLowerCase().includes(this.state.filter)
-        ),
-      }));
-    } else {
-      this.setState({
-        contacts: [
-          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-        ],
-        filter: "",
-      });
-    }
+    this.setState({
+      filter: e.target.value,
+    });
   };
 
   formSubmitHandler = (data) => {
+    console.log(data);
     this.setState((prevState) => ({
       contacts: [data, ...prevState.contacts],
     }));
   };
 
+  getVisibleList = () => {
+    const { filter, contacts } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
   render() {
-    const { contacts, filter } = this.state;
+    const { filter } = this.state;
+
+    const getVisibleContacts = this.getVisibleList();
+
     return (
       <div className="App">
         <h1 className="titlePhonebook">Phonebook</h1>
         <ContactForm formSubmitHandler={this.formSubmitHandler} />
         <h1 className="titleContacts">Contacts</h1>
 
-        <p>Find contacts by name</p>
-        <input
-          type="text"
-          name="filter"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-          className="inputFilter"
-          onChange={this.filterList}
-          value={filter}
-        />
+        <Filter filter={filter} filterList={this.filterList} />
 
         <ul className="listContacts">
-          {contacts.map((contact) => (
+          {getVisibleContacts.map((contact) => (
             <li key={contact.id}>
               {contact.name}: {contact.number}
             </li>
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import css from "./Filter.module.css";
+
+function Filter({ filter, filterList }) {
+  console.log(filter);
+  return (
+    <div>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        className={css.inputFilter}
+        onChange={filterList}
+        value={filter}
+      />
+    </div>
+  );
+}
+
+Filter.propTypes = {
+  filter: PropTypes.string,
+  formSubmitHandler: PropTypes.func,
+};
+
+export default Filter;
